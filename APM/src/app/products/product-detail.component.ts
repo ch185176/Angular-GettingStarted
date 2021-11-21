1157c5ca8062e5e3184722d5e53b555e7a8a18e3
Craig Hutcheon <craighutcheon333@hotmail.com>
2021-11-21 20:04:20 +0000

    Product Detail Implementation

--- APM/src/app/products/product-detail.component.ts
+++ APM/src/app/products/product-detail.component.ts
@@ -8,7 +8,19 @@ import { IProduct } from './product';
 })
 export class ProductDetailComponent implements OnInit {
   pageTitle = 'Product Detail';
-  product: IProduct | undefined;
+  product: IProduct = 
+  {
+    "productId": 1,
+    "productName": "Leaf Rake",
+    "productCode": "GDN-0011",
+    "releaseDate": "March 19, 2021",
+    "description": "Leaf rake with 48-inch wooden handle.",
+    "price": 19.95,
+    "starRating": 3.2,
+    "imageUrl": "assets/images/leaf_rake.png"
+  };
+  imageWidth = 200;
+  imageMargin = 2;
 
   constructor(private route: ActivatedRoute, private router: Router) { }
 
